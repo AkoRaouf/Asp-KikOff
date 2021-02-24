8542bd0c145355568273c70e48218418b147bea1
Raouf <acer@DESKTOP-HMQQE1N>
2021-02-24 08:43:59 +0330

    Add a few validation attribuutes

--- ASP.NET.Core.WebApp.ModelValidation/Models/Appointment.cs
+++ ASP.NET.Core.WebApp.ModelValidation/Models/Appointment.cs
@@ -5,9 +5,15 @@ namespace ASP.NET.Core.WebApp.ModelValidation.Models
 {
     public class Appointment
     {
+        [Required]
+        [Display(Name ="name")]
         public string ClientName { get; set; }
+
         [UIHint("Date")]
+        [Required(ErrorMessage ="Please enter the date")]
         public DateTime? Date { get; set; }
+
+        [Range(typeof(bool), "true", "true", ErrorMessage = "You must accept the terms"]
         public bool TermsAccepted { get; set; }
     }
 }
